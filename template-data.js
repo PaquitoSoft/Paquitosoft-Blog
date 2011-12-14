0936f00e356bdb1296b7c5f66551e683f11ce2fa
PaquitoSoft <paquitosoftware@gmail.com>
2011-12-14 16:02:58 +0000

    Creating projects model

--- template-data.js
+++ template-data.js
@@ -28,6 +28,47 @@ var post2 = {
 	tags: ["development", "javascript"]
 };
 
+var projects = [{
+    name: "Paquitometer",
+    title: "Network traffic monitor",
+    description: "Desktop application written with [**GAMBAS**](http://gambas.sourceforge.net/en/main.html) to run on linux machines.",    
+    publishingDate: new Date(2005,5,8),
+    url: "http://sourceforge.net/projects/paquito-meter/",
+    features: ["Monitor yor incoming and outgoing traffic", "Historical statistics of your network usage"]
+},
+{
+    name: "Namtia",
+    title: "Music player",
+    description: "Namtia is not only a music player, it's also a way of manage your songs library.",
+    publishingDate: new Date(2006,11,26),
+    url: "http://sourceforge.net/projects/namtia/",
+    features: ["Play your music files", "Organize your library", "View songs lyrics", "Easy searching/browsing capabilities"]
+},
+{
+    name: "LML",
+    title: "Java annotations persistence library",
+    description: "This is a Java library to help developers when working with relational databases. Based on annotations, you just have to decorate your POJOs to handle its persistent state.",
+    publishingDate: new Date(2008, 4, 8),
+    url: "http://sourceforge.net/projects/lml/",
+    features: ["Annotation based", "Use your own POJOs", "Very small", "Easy to use"]
+},
+{
+    name: "Notimoo",
+    title: "Wen Growl style notifications",
+    description: "This is a [Mootools](http://www.mootools.net) plugin for web developers so they can show asynchronous notifications to their users",
+    publishingDate: new Date(2009,12,12),
+    url: "https://github.com/PaquitoSoft/Notimoo",
+    features: ["Great out-of-the-box defaults", "Highly customizable"]
+},
+{
+    name: "Notiquery",
+    title: "Wen Growl style notifications",
+    description: "This is a [jQuery](http://wwwjquery.com) plugin for web developers so they can show asynchronous notifications to their users",
+    publishingDate: new Date(2011,11,18),
+    url: "https://github.com/PaquitoSoft/Notiquery",
+    features: ["Great out-of-the-box defaults", "Highly customizable"]
+}];
+
 exports.initializeDatabase = function(models) {
 	var user1 = new models.User(user);
 	user1.save(function(err) {
@@ -49,5 +90,14 @@ exports.initializeDatabase = function(models) {
 			}
 		});	
 	}, 1000);
-	
-}
+    var project;
+    for (var index = 0; index < projects.length; index++) {
+        project = new models.Project(projects[index]);
+        project.save(function(err) {
+            if (err) {
+				logger.error("There was an error creating a sample project.", err);
+			}
+        });
+    }
+};
+    
